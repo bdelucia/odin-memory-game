6d4b66b16d7da3508f36d135918f2b075068a626
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-05-19 16:51:24 -0700

    feat: card flipping, backside of card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,44 @@
 import '../index.css';
+import Tilt from 'react-parallax-tilt';
+import { useState } from 'react';
+import ReactCardFlip from 'react-card-flip';
+import PokeBall from '../assets/pokeball.svg';
 
 function Card({ name, sprite }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
   return (
-    <div className="card bg-primary w-96 shadow-sm">
-      <figure>
-        <img src={sprite} alt={name} />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title flex justify-center align-middle">{name}</h2>
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      <div onClick={() => setIsFlipped(true)} key="front">
+        <Tilt className="cursor-pointer">
+          <div className="card bg-primary w-96 h-48 shadow-sm">
+            <figure>
+              <img src={sprite} alt={name} />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title flex justify-center align-middle">
+                {name}
+              </h2>
+            </div>
+          </div>
+        </Tilt>
       </div>
-    </div>
+
+      <div onClick={() => setIsFlipped(false)} key="back">
+        <Tilt className="cursor-pointer">
+          <div className="card bg-primary w-96 h-48 shadow-sm">
+            <figure>
+              <img src={PokeBall} className="w-24 h-24" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title flex justify-center align-middle italic">
+                Bobbeh's Pokemon Memory Game
+              </h2>
+            </div>
+          </div>
+        </Tilt>
+      </div>
+    </ReactCardFlip>
   );
 }
 
